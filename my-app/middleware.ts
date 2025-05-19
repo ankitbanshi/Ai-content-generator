7805db128da3100f34d3c871adb28ffbb6022970
ankitbanshi <banshiwalankit448@gmail.com>
2025-05-19 18:38:14 +0530

    create side navigation bar and header

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server'
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
 })
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)',"/"])
+const isProtectedRoute = createRouteMatcher(['/','/dashboard(.*)'])
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
